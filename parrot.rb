50fd0cfb29788bb1346b8d98b51f85ba64379ef7
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-18 20:01:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  put phrase
-  "Squawk!"
+  puts phrase
+  return phrase
+  #"Squawk!"
 end 
